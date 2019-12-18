1fd7e761728b45bfcdc887f15f6a51f41415b2ac
DOligex <olinours@yahoo.fr>
2019-12-18 21:18:52 +0100

    fixe: update name

--- index1.js
+++ index1.js
@@ -1,4 +1,4 @@
-// incrémenter l'ensemble des projets dans l'objet data
+
 // création d'un tableau data contenant deux cles 
 // 1 class avec valeur les classes html déjà appelés dans un fichier css
 // 2 text avec valeur les textes du fichier html
@@ -27,41 +27,27 @@ for (const d of data1) {
     <h2 class="projectstitle">NOS DIFFERENTS PROJETS</h2>
     <section class="projectspart1">
     <article class="projectsimgpart1">
-      <p class="pictitle">MARIAGE</p>
+    <p class="pictitle">MARIAGE</p>
     </article>
     <article class="projectsimgpart2">
     <p class="pictitle">BAR-MITZVA</p>
     </article>
     </section>
     <section class="projectspart2">
-      <article class="projectsimgpart3">
-        <p class="pictitle">BAPTEME</p>
-      </article>
-      <article class="projectsimgpart4">
-        <p class="pictitle">SOIREE ENTRE AMIS</p>
-      </article>
+    <article class="projectsimgpart3">
+    <p class="pictitle">BAPTEME</p>
+    </article>
+    <article class="projectsimgpart4">
+    <p class="pictitle">SOIREE ENTRE AMIS</p>
+    </article>
     </section>
     <section class="projectspart3">
-      <article class="projectsimgpart5">
-        <p class="pictitle">SOIREE D'ENTREPRISE</p>
-      </article>
-      <article class="projectsimgpart6">
-        <p class="pictitle">ARTISTIQUE</p>
-      </article>
+    <article class="projectsimgpart5">
+    <p class="pictitle">SOIREE D'ENTREPRISE</p>
+    </article>
+    <article class="projectsimgpart6">
+    <p class="pictitle">ARTISTIQUE</p>
+    </article>
     </section>
     `
 }
-
-// let data2 = [
-//     {class: "table",
-//     text: "Option livre prestige"},
-
-// ]
-// const container2 = document.getElementById('prestige');
-
-// for (const e of data2) {
-//     const article = document.getElementById('');
-//     container.innerHTML=`
-//     <h3>Option livre prestige</h3>
-//     `
-// }
---
+++ index2.js
@@ -0,0 +1,12 @@
+
+// la modal a afficher
+const modal = document.getElementById('modal');
+// le boutton a cliquer pour ouvrir
+const openBtn = document.getElementById('open-modal-test');
+openBtn.addEventListener('click', ()=>modal.style.display = 'block');
+const closeBtn = document.getElementById('close-modal-test');
+closeBtn.addEventListener('click', ()=>modal.style.display= '');
+
+const modal2 = document.getElementById('modal2');
+
+const openBtn2 = document.getElementById('')
